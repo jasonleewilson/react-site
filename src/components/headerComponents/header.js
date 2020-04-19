699ff8ff1a3ad2a4933ddea17a9410defe294587
jasonleewilson <info@jasonleewilson.com>
2020-04-19 04:35:34 -0500

    update styles

--- src/components/headerComponents/header.js
+++ src/components/headerComponents/header.js
@@ -4,15 +4,15 @@ function Header() {
   return (
     <header>
         
-        <div clasName="logo">
+        <div className="logo">
             LOGO
         </div>
 
         <nav>
             <ul>
-                <li><a href="#0">Home</a></li>
-                <li><a href="#0">Home</a></li>
-                <li><a href="#0">Home</a></li>
+                <li className="first"><a href="#0">Home</a></li>
+                <li><a href="#0">About</a></li>
+                <li className="last"><a href="#0">Contact</a></li>
             </ul>
         </nav>
 
